0e4429dd15878572f5e568d5adbb6b5fa5f874b6
wellWINeo <usa200313@gmail.com>
2020-10-05 22:41:23 +0300

    logs module improved

--- logs/log.py
+++ logs/log.py
@@ -1,5 +1,44 @@
 
 # here will be funcs for logs
+class log():
+    
+    #TODO in future lenght will be obtained from config file
+    def __init__(self, file_name, lenght = 10):
+        self.max_lenght = lenght
+        
+        self.log_handle = open(file_name, "rt")
+        self.file_name = file_name
 
-def write(text):
-    print(text)
+        if self.__get_lines_count() >= lenght:
+            self.log_handle.close()
+            self.log_handle = open(self.file_name, "wt")
+            self.log_handle.write("")
+
+    def __change_mode(self, mode):
+        if self.log_handle.mode != mode:
+            self.log_handle.close()
+            self.log_handle = open(self.file_name, mode)
+
+
+    def __get_lines_count(self):
+        count = 0
+        if "r" not in self.log_handle.mode:
+            self.__change_mode("rt")
+
+        for line in self.log_handle:
+            count+= 1
+        
+        return count
+        
+    def write(self, text):
+        print(text)
+        if self.log_handle.mode != "a":
+            self.log_handle.close()
+            self.log_handle = open(self.file_name, "a")
+        self.log_handle.write(text + "\n")
+
+
+if __name__ == "__main__":
+    log = log("sys_logs.txt")
+    log.write("[test] Test log")
+
