c887b8010febcbfe8cf277618f86cf3b6b513c60
wellWINeo <usa200313@gmail.com>
2020-11-07 15:08:23 +0300

    command handling improved

--- bot/core_bot.py
+++ bot/core_bot.py
@@ -2,13 +2,18 @@ import telebot
 import time
 import config
 import bot_markup
-import math
 from mptParser.mptShedule import mptPage
 
-users = {}
-bot = telebot.TeleBot(config.bot_token)
-bot_command=["start", "расписание"]
+users = []
+tg_bot = telebot.TeleBot(config.bot_token)
 mpt = mptPage()
+
+commands_tree = {
+        "START": ["start", "/start"],
+        "SHEDULE": ["shedule", "/shedule", "расписание", "/расписание"],
+        "CHANGES": ["changes", "/changes", "замены", "/замены"],
+        "HELP": ["help", "/help", "помощь", "/помощь"]}
+
 
 class user():
     user_id = None
@@ -19,81 +24,80 @@ class user():
         self.user_id = _id
         self.name = _name
 
-def is_user_known(user_id):
+def recognize_user(id_):
     for user in users:
-        print(user)
-        print(type(user))
-        if type(user) != 'int' and user.user_id == user_id:
-            return True
+        if user.user_id == id_:
+            return user
     return False
 
 
-@bot.message_handler(commands=["start"])
+@tg_bot.message_handler(func=lambda message: 
+                        True if message.text in commands_tree["START"] 
+                        else False)
 def start_handler(message):
-    bot.reply_to(message, "Привет, бот запущен!\nЕго цель - удобный просмотр расписания МПТ, \
+    tg_bot.reply_to(message, "Привет, бот запущен!\nЕго цель - удобный просмотр расписания МПТ, \
 а также он предупреждает о заменах.\n \
 Для того чтобы узнать о комманда введите:\n \
 /help")
-    if not is_user_known(message.from_user.id):
-        bot.send_message(message.chat.id, "Выберите направление: ",
+    if recognize_user(message.from_user.id) == False:
+        tg_bot.send_message(message.chat.id, "Выберите направление: ",
             reply_markup=bot_markup.direction_choose_keyboard())
 
-        new_user = user(message.chat.id, message.from_user.first_name)
-        users.update({new_user.user_id : new_user})
-        bot.set_update_listener(wait_for_dir_answer)
-
-
+        new_user = user(message.from_user.id, message.from_user.first_name)
+        users.append(new_user) 
+        tg_bot.set_update_listener(wait_for_dir_answer)
 
 def wait_for_dir_answer(messages):
     
     for message in messages:
-        
-        if message.chat.id in users:
-          
-            if (users[message.chat.id]).group == None:
-                bot.send_message(message.chat.id, "Выберите группу: ",
-                        reply_markup=bot_markup.group_choose_keyboard(mpt, message.text))
-                users[message.chat.id].group = "-"
-           
-            elif (users[message.chat.id]).group == "-":
-                bot.send_message(message.chat.id, "Отличо! Группа выбрана и сохранена")
-                users[message.chat.id].group = message.text
-            
-            #else:
-            #   bot.send_message(message.chat.id, "Такой команды я не знаю(")
+        gen = (user for user in users if user.user_id == message.from_user.id)
+        for user in gen:       
+            if user.group == None: 
+                tg_bot.send_message(message.chat.id, "Выберите группу: ",
+                                    reply_markup=bot_markup.group_choose_keyboard(mpt, message.text))
+                user.group = "-"
+
+            elif user.group == "-": 
+                tg_bot.send_message(message.chat.id, "Отличо! Группа выбрана и сохранена")
+                user.group = message.text 
 
 
-@bot.message_handler(commands=["расписание"])
+@tg_bot.message_handler(func=lambda message:
+                        True if message.text in commands_tree["HELP"]
+                        else False)
+def help_handler(message):
+    tg_bot.send_message(message.chat.id, "help_placeholder")
+
+
+@tg_bot.message_handler(func=lambda message:
+                        True if message.text in commands_tree["SHEDULE"]
+                        else False)
 def shedule_handler(message):
-    bot.send_message(message.chat.id, "Выберите на какой срок: ", 
+    tg_bot.send_message(message.chat.id, "Выберите на какой срок: ", 
             reply_markup=bot_markup.choose_shedule_date())
 
-@bot.callback_query_handler(func= lambda call: True if call.data[:3] == "cb_" else False)
+
+@tg_bot.callback_query_handler(func=lambda call: 
+                               True if call.data[:3] == "cb_" 
+                               else False)
 def callback_query(call):
+    cur_user = recognize_user(call.from_user.id)
+    
     if call.data == "cb_today":
-        bot.answer_callback_query(call.id, "Расписание на сегодня")
-        bot.send_message(call.message.chat.id, "Today_placeholder")
+        tg_bot.answer_callback_query(call.id, "Расписание на сегодня")
+        tg_bot.send_message(call.message.chat.id, "Today_placeholder {0}".format(cur_user.group))
     
     elif call.data == "cb_tomorrow":
-        bot.answer_callback_query(call.id, "Расписание на завтра")
-        bot.send_message(call.message.chat.id, "Tommorow_placeholer")
+        tg_bot.answer_callback_query(call.id, "Расписание на завтра")
+        tg_bot.send_message(call.message.chat.id, "Tommorow_placeholer {0}".format(cur_user.group))
     
     elif call.data == "cb_week":
-        bot.answer_callback_query(call.id, "Расписание на неделю")
-        bot.send_message(call.message.chat.id, "Weak_placeholder " + users[call.message.chat.id].group)
+        tg_bot.answer_callback_query(call.id, "Расписание на неделю")
+        tg_bot.send_message(call.message.chat.id, "Weak_placeholder {0}".format(cur_user.group))
 
     else:
-        bot.answer_callback_query(call.id, "Internal callback function error")
-
-
-#@bot.message_handler(func= lambda message: True if message not in bot_command else False)
-#def incorrect_command_handler(message):
-#    bot.send_message(message.chat.id, "Такой команы я не знаю(")
+        tg_bot.answer_callback_query(call.id, "Internal callback function error")
 
 
 if __name__ == "__main__":
-    bot.polling()
-
-
-
-
+    tg_bot.polling()
